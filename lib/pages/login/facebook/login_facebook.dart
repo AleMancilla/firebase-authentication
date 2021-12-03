ee60b299e213e88703716a2927cfbfb6b504bb04
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2021-12-02 21:38:44 -0400

    facebook not found for sha

--- lib/pages/login/facebook/login_facebook.dart
+++ lib/pages/login/facebook/login_facebook.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/utils.dart';
 import 'package:firebase_authentication/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class LoginFacebook extends StatelessWidget {
   const LoginFacebook({Key? key}) : super(key: key);
@@ -9,8 +12,39 @@ class LoginFacebook extends StatelessWidget {
     return Container(
       color: Colors.purple.shade200,
       child: Center(
-        child: ButtonWidget(textButton: 'login Facebook', ontap: () {}),
+        child: ButtonWidget(
+          textButton: 'login Facebook',
+          ontap: () async {
+            try {
+              await signInWithFacebook();
+            } catch (e) {
+              print('#############################');
+              print(e);
+              print('#############################');
+              snackBarMessageError(context, e.toString());
+            }
+          },
+        ),
       ),
     );
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    try {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+      print('----- status ${loginResult.status}');
+      print('----- status ${loginResult.accessToken}');
+      print('----- status ${loginResult.message}');
+
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+      // Once signed in, return the UserCredential
+      return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    } catch (e) {
+      return throw 'ERROR = $e';
+    }
   }
 }
